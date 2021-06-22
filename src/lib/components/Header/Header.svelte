db2cabd3354fe54131eebec9466d6c63af7a5ac5
Schaeferjo <mail@joschaefer.com>
2021-06-22 15:58:45 +0200

    feat: working on headerstyle

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -7,7 +7,7 @@
   const linkClass = (path, pge) => {
     const defaults = 'inline-block no-underline py-2 px-4';
     const active = 'text-white';
-    const inactive = 'text-gray-600 hover:text-gray-200 hover:text-underline';
+    const inactive = 'text-white hover:text-gray-200 hover:text-underline';
     return `${pge.path === path ? active : inactive} ${defaults}`;
   };
 
@@ -26,31 +26,31 @@
   });
 </script>
 
-<header class="sticky top-0 z-10">
-  <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 w-full">
+<header class="sticky top-0 z-10 bg-ti_blue_mat shadow-lg">
+  <nav class="container flex items-center justify-between flex-wrap px-6 py-4 mx-auto w-full">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <a
-        class="flex items-center text-white no-underline hover:text-white hover:no-underline"
+        class="flex items-center text-white no-underline"
         sveltekit:prefetch
         href="{base}/"
       >
-        <div class="w-12 border-2"><Logo /></div>
-        <span class="text-2xl pl-2">Tanzimpulse</span>
+        <div class="w-20"><Logo /></div>
       </a>
     </div>
 
-    <div class="block lg:hidden">
+    <div class="block self-align-end">
       <button
         on:click={toggleMenu}
-        class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+        class="flex items-center px-3 py-2 focus:outline-none focus:border-0 text-ti_blue_accent hover:text-white"
       >
-        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+      <span class="uppercase font-extrabold px-2">Menu</span>
+        <svg class="fill-current h-6 w-6 text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
           ><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg
         >
       </button>
     </div>
 
-    <div
+    <!--<div
       class:hidden={!open}
       class="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block pt-6 lg:pt-0"
     >
@@ -61,6 +61,12 @@
           </li>
         {/each}
       </ul>
-    </div>
+    </div>-->
   </nav>
 </header>
+
+
+
+
+
+

e3bbed5481e1dedc0ed92c42466938d39fc4a760
Schaeferjo <mail@joschaefer.com>
2021-06-25 11:41:42 +0200

    feat: basic layout styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,7 +17,7 @@
       <div class="___card1 bg-ti_blue_mat p-8 text-white shadow-sm hover:shadow-lg flex-1">
         <h2 class="text-ti_blue_accent font-bold text-2xl uppercase tracking-wide">Die Ausbildungen</h2>
         <p class="pt-4 pb-8">Bilden Sie sich mit unseren Zusatzqualifikationen zur Tanzpädagogin oder zum Tanzpädagogen weiter. Weitere wichtige Tipps und Hinweise, die das Angebot schmackhaft machen.</p>
-        <button class="p-4 mb-12 w-full rounded-full border-2 border-ti_blue_accent text-ti_blue_accent font-black text-sm tracking-widest uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-ti_blue_mat-dark transition duration-300 ease-in-out">Mehr erfahren</button>
+        <button class="p-4 mb-12 w-full rounded-full border-2 border-ti_blue_accent bg-ti_blue_mat-dark text-ti_blue_accent font-black text-sm tracking-widest uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-white transition duration-300 ease-in-out">Mehr erfahren</button>
         
         <!-- startpage single seminar -->
         <div class="___start_single_seminar_teaser flex flex-row lg:flex-col xl:flex-row gap-x-4 items-center">
@@ -27,7 +27,7 @@
             <p class="___startpage_seminar_status py-2 uppercase text-ti_green_accent-light text-sm font-black tracking-wider">Noch Plätze frei</p>
           </div>
           <div class="___startpage_seminar_more_icon flex-0 self-align-center">
-           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-ti_blue_mat-dark transition duration-300 ease-in-out">
+           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent bg-ti_blue_mat-dark font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-white transition duration-300 ease-in-out">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
@@ -45,7 +45,7 @@
             <p class="___startpage_seminar_status py-2 uppercase text-sm font-bold tracking-wider">– Warteliste -</p>
           </div>
           <div class="___startpage_seminar_more_icon flex-0 self-align-center">
-           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-ti_blue_mat-dark transition duration-300 ease-in-out">
+           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent bg-ti_blue_mat-dark font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-white transition duration-300 ease-in-out">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
@@ -63,7 +63,7 @@
             <p class="___startpage_seminar_status py-2 uppercase text-ti_green_accent-light text-sm font-black tracking-wider">Noch Plätze frei</p>
           </div>
           <div class="___startpage_seminar_more_icon flex-0 self-align-center">
-           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-ti_blue_mat-dark transition duration-300 ease-in-out">
+           <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_blue_accent text-ti_blue_accent bg-ti_blue_mat-dark font-bold text-sm tracking-wide uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-white transition duration-300 ease-in-out">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
@@ -78,8 +78,8 @@
 
       <div class="___card2 bg-ti_cyan_mat p-8 text-white shadow-sm hover:shadow-lg flex-1">
         <h2 class="text-ti_cyan_accent font-bold text-2xl uppercase tracking-wide">Die Workshops</h2>
-        <p class="pt-4 pb-8">Integrieren Sie neue tanzpädagogischeImpulse in Ihren Arbeitsalltag.Ideal für Erzieher, Lehrer, Therapeuten, Übunugsleiter und viele mehr!</p>
-        <button class="p-4 mb-12 w-full rounded-full border-2 border-ti_cyan_accent text-ti_cyan_accent font-black text-sm tracking-widest uppercase hover:border-ti_blue_cyan-light hover:text-ti_cyan_accent-light hover:bg-ti_cyan_mat-dark transition duration-300 ease-in-out">Mehr erfahren</button>
+        <p class="pt-4 pb-8">Integrieren Sie neue tanzpädagogischeImpulse in Ihren Arbeitsalltag.Ideal für Erzieher, Lehrer, Therapeuten, Übungsleiter und viele mehr!</p>
+        <button class="p-4 mb-12 w-full rounded-full border-2 border-ti_cyan_accent text-ti_cyan_accent bg-ti_cyan_mat-dark font-black text-sm tracking-widest uppercase hover:border-ti_blue_cyan-light hover:text-ti_cyan_accent-light hover:bg-white transition duration-300 ease-in-out">Mehr erfahren</button>
         
           <!-- startpage single seminar -->
           <div class="___start_single_seminar_teaser flex flex-row lg:flex-col xl:flex-row gap-x-4 items-center">
@@ -89,7 +89,7 @@
               <p class="___startpage_seminar_status py-2 uppercase text-ti_green_accent-light text-sm font-black tracking-wider">Noch Plätze frei</p>
             </div>
             <div class="___startpage_seminar_more_icon flex-0 self-align-center">
-              <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_cyan_accent text-ti_cyan_accent font-bold text-sm tracking-wide uppercase hover:border-ti_cyan_accent-light hover:text-ti_cyan_accent-light hover:bg-ti_cyan_mat-dark transition duration-300 ease-in-out">
+              <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_cyan_accent text-ti_cyan_accent bg-ti_cyan_mat-dark font-bold text-sm tracking-wide uppercase hover:border-ti_cyan_accent-light hover:text-ti_cyan_accent-light hover:bg-white transition duration-300 ease-in-out">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
               </svg>
@@ -107,7 +107,7 @@
               <p class="___startpage_seminar_status py-2 uppercase text-sm font-bold tracking-wider">– Warteliste -</p>
             </div>
             <div class="___startpage_seminar_more_icon flex-0 self-align-center">
-              <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_cyan_accent text-ti_cyan_accent font-bold text-sm tracking-wide uppercase hover:border-ti_cyan_accent-light hover:text-ti_cyan_accent-light hover:bg-ti_cyan_mat-dark transition duration-300 ease-in-out">
+              <button class="rounded-full h-12 w-12 flex items-center justify-center border-2 border-ti_cyan_accent text-ti_cyan_accent bg-ti_cyan_mat-dark font-bold text-sm tracking-wide uppercase hover:border-ti_cyan_accent-light hover:text-ti_cyan_accent-light hover:bg-white transition duration-300 ease-in-out">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
               </svg>
@@ -123,7 +123,7 @@
       <div class="___card3 bg-ti_green_mat p-8 text-white shadow-sm hover:shadow-lg flex-1">
         <h2 class="text-ti_green_accent font-bold text-2xl uppercase tracking-wide">Der (Tanz-) Raum</h2>
         <p class="pt-4 pb-8">Alle Informationen zum Tanzraum. Den kann man nach Rücksprache auch mieten für Veranstaltungen, wie Lesungen, etc.</p>
-        <button class="p-4 w-full rounded-full border-2 border-ti_green_accent text-ti_green_accent font-black text-sm tracking-widest uppercase hover:border-ti_blue_green-light hover:text-ti_green_accent-light hover:bg-ti_green_mat-dark transition duration-300 ease-in-out">Mehr erfahren</button>
+        <button class="p-4 w-full rounded-full border-2 border-ti_green_accent text-ti_green_accent bg-ti_green_mat-dark font-black text-sm tracking-widest uppercase hover:border-ti_blue_green-light hover:text-ti_green_accent hover:bg-white transition duration-300 ease-in-out">Mehr erfahren</button>
       </div>    
 
     </div>
@@ -133,7 +133,7 @@
   <h2 class="___default_h2_headline font-bold text-md lg:text-2xl text-ti_blue_mat">Default Headline h2</h2>
   <p class="___default_plain_text">Lorem ipsum</p>
   <p class="___default_status py-2 uppercase text-ti_green_accent text-sm font-black tracking-wider">Noch Plätze frei</p>
-  <button class="___default_button p-4 my-6 w-full rounded-full border-2 border-ti_blue_mat text-ti_blue_mat font-black text-sm tracking-widest uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-ti_blue_mat-dark transition duration-300 ease-in-out">Mehr erfahren</button>
+  <button class="___default_button p-4 my-6 w-full rounded-full border-2 border-ti_blue_accent text-ti_blue_accent-light bg-ti_blue_mat-dark font-black text-sm tracking-widest uppercase hover:border-ti_blue_accent-light hover:text-ti_blue_accent-light hover:bg-white transition duration-300 ease-in-out">Mehr erfahren</button>
 
 </div>
 

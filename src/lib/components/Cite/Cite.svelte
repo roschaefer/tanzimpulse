6f0887d5b4b52387cf0b578d0660ccde2cd72414
Schaeferjo <mail@joschaefer.com>
2021-06-28 10:50:25 +0200

    feat: cite component

---
+++ src/lib/components/Cite/Cite.svelte
@@ -0,0 +1,7 @@
+<div class="___cite flex flex-col w-full py-12">
+  <img class="w-20 h-20 mx-auto mb-6 rounded-full shadow-md" src="./images/employees/woehler_sabrina.jpg" alt="Sabrina Wöhler | Tanzimpulse" />
+  <p class="___author mx-auto uppercase tracking-widest text-warmGray-500 text-sm"><slot name="name"></slot></p>
+  <cite class="mx-auto px-8 py-4 text-ti_blue_mat text-3xl font-light not-italic">
+   <slot name="cite"></slot>
+  </cite>
+</div>

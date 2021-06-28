0e2c01a0f7779fc6263f3f80012b7d5760bfab61
Schaeferjo <mail@joschaefer.com>
2021-06-28 20:02:24 +0200

    feat: minor stylings

--- src/lib/components/Cite/Cite.svelte
+++ src/lib/components/Cite/Cite.svelte
@@ -1,7 +1,7 @@
-<div class="___cite flex flex-col w-full py-12">
-  <img class="w-20 h-20 mx-auto mb-6 rounded-full shadow-md" src="./images/employees/woehler_sabrina.jpg" alt="Sabrina Wöhler | Tanzimpulse" />
-  <p class="___author mx-auto uppercase tracking-widest text-warmGray-500 text-sm"><slot name="name"></slot></p>
-  <cite class="mx-auto px-8 py-4 text-ti_blue_mat text-3xl font-light not-italic">
+<div class="___cite flex flex-col w-full py-12 px-4 lg:px-8 ">
+  <img class="w-20 h-20 mx-auto mb-4 rounded-full shadow-md" src="./images/employees/woehler_sabrina.jpg" alt="Sabrina Wöhler | Tanzimpulse" />
+  <p class="___author mx-auto px-8 uppercase tracking-widest text-warmGray-500 text-sm"><slot name="name"></slot></p>
+  <cite class="mx-auto py-6 text-ti_blue_mat text-3xl font-light not-italic">
    <slot name="cite"></slot>
   </cite>
 </div>

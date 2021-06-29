eda688f283edcd3e916c3b5cb8744ee1b997384c
Schaeferjo <mail@joschaefer.com>
2021-06-29 12:32:54 +0200

    feat: add several button styles

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,13 +1,16 @@
 <script>
-	export let buttonstyle = 'red'
+	export let buttonstyle = 'blue'
 
   const mapping={
-    red: 'text-ti_red_accent border-ti_red_accent', 
-    blue: 'text-ti_blue_accent border-ti_blue_accent', 
+    blue: 'text-ti_blue_mat border-ti_blue_mat hover:border-ti_blue_accent hover:text-ti_blue_accent', 
+    red: 'text-ti_red_mat border-ti_red_mat hover:border-ti_red_accent hover:text-ti_red_accent',  
+    green: 'text-ti_green_mat border-ti_green_mat hover:border-ti_green_accent hover:text-ti_green_accent',
+    gray: 'text-gray-400 border-gray-400 cursor-not-allowed', 
+ 
   }  
 </script>
 
 
-<button class="___default_button {mapping[buttonstyle]} bg-white p-4 my-6 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase transition duration-300 ease-in-out">
+<button class="___default_button {mapping[buttonstyle]} bg-white p-4 my-6 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase outline-none focus:outline-none transition duration-300 ease-in-out">
   <slot></slot>
 </button>

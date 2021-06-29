78167069c3e65caf702cda5567930134ee670311
Schaeferjo <mail@joschaefer.com>
2021-06-29 15:12:35 +0200

    feat: new page impressum and datenschutz

---
+++ src/routes/datenschutz.svelte
@@ -0,0 +1,91 @@
+<script context="module" lang="ts">
+  import { base } from '$app/paths';
+  import DefaultCard from '$lib/components/DefaultCard/DefaultCard.svelte';
+  import Ausbildungen from './ausbildungen.svelte';
+</script>
+
+<svelte:head>
+  <title>Datenschutz</title>
+</svelte:head>
+
+<section>
+  <div class="container">
+    <div class="grid grid-cols-1 gap-10">
+      <DefaultCard>
+        <h2 class="text-2xl uppercase font-extrabold text-ti_blue_accent tracking-wide">Datenschutz</h2>
+        <h2 class="text-2xl uppercase text-ti_blue_mat tracking-wide mb-4">TANZIMPULSE – Institut für Tanzpädagogik</h2>
+        <p class="py-2">
+          Folwiese 20<br />
+          51069 Köln
+        </p>
+        <p class="py-2">
+          Telefon: +49 (0)221-68 36 91
+          <br />
+          Fax: 0221-68 97 79 7
+          <br />
+          E-Mail: <a class="font-bold pt-2 uppercase text-xs tracking-wide hover:text-ti_blue_accent ease-in-out transition-all duration-300" href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a>
+        </p>
+        <h3 class="text-xl text-ti_blue_mat py-4">1.) Name und Abschrift des Verantwortlichen</h3>
+        <p>Mit dem 25. Mai 2018 gilt die neue EU-Datenschutzgrundverordnung (DSGVO). Durch die DSGVO werden Ihre Datenschutzrechte weiter gestärkt. Wichtig ist die Erhöhung der Transparenz bei der Verarbeitung personenbezogener Daten. In diesem Zusammenhang informieren wir Sie über unsere Maßnahmen zum Schutz Ihrer personenbezogenen Daten.</p>
+        <p class="py-2">Die Verantwortliche im Sinne der Datenschutz-Grundverordnung (DSGVO) und anderer nationaler Datenschutzgesetze der Mitgliedsstaaten sowie sonstiger datenschutzrechtlicher Bestimmungen ist</p>
+        <p class="py-2">
+          TANZIMPULSE – Institut für Tanzpädagogik GbR<br />
+          Künstlerische Leitung: Bettina Bierdümpel / Geschäftsführerin: Ute Luckey<br />
+          Büroanschrift: Folwiese 20, 51069 Köln<br />
+          Gerichtsstand: Köln<br />
+        </p>
+        <h3 class="text-xl text-ti_blue_mat py-4">2.) Allgemeines zur Datenverarbeitung</h3>
+        <p class="font-bold">a) Umfang der Verarbeitung personenbezogener Daten</p>
+        <p>TANZIMPULSE GbR behandelt die von Ihnen genannten personenbezogenen Daten gemäß den Vorschriften der DSGVO und nutzt diese zum Zweck:</p>
+        <ul class="px-8 py-2 list-disc">
+          <li>der Vertragsabwicklung</li>
+          <li>von Rechnungsstellung</li>
+          <li>oder notwendiger Informationen</li>
+        </ul>
+        <p>Dieser Vertrag kommt durch Ihre Anmeldung über unsere Webseite, per E-Mail, telefonisch oder postalisch zustande.</p>
+        <p class="font-bold mt-6">b) Gespeicherte Daten</p>
+        <p>Die lokale Speicherung persönlicher Daten erfolgt bei :</p>
+        <ul class="px-8 py-2 list-disc">
+          <li>Anmeldung zu einem Workshop</li>
+          <li>zu einem Kurs</li>
+          <li>zur Zusatzqualifikation Tanzpädagogik oder</li>
+          <li>Informationsinteresse</li>
+        </ul>
+        <p>Namen, Adresse, Telefonnummern und E-Mailkontakt.</p>
+        <p class="font-bold mt-6">c) Recht auf Auskunft</p>
+        <p>Sie können jederzeit Auskunft über den Umfang der Speicherung Ihrer persönlichen Daten erhalten.</p>
+        <p class="font-bold mt-6">d) Recht auf Berichtigung</p>
+        <p class="py-2">Sollten Ihre persönlichen Daten unrichtig oder unvollständig sein, nehmen wir unverzüglich eine Korrektur vor.</p>
+        <p class="font-bold mt-6">e) Recht auf Löschung</p>
+        <p class="py-2">
+          Sie können jederzeit schriftlich (auch per E-Mail) die Löschung Ihrer personenbezogenen Daten beantragen (Art. 17 DSGVO), sofern keine Notwendigkeit für eine Speicherung besteht.
+          <br />
+          Bitte E-Mail an: <a class="font-bold pt-2 uppercase text-xs tracking-wide hover:text-ti_blue_accent ease-in-out transition-all duration-300" href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a>
+        </p>
+        <p class="font-bold mt-6">f) Weitergabe personenbezogener Daten</p>
+        <p class="py-2">
+          Ihre persönlichen Daten werden grundsätzlich nicht an Dritte weitergegeben. Eine Weitergabe erfolgt nur wenn dies zur Erfüllung unserer Arbeit erforderlich ist.
+          <br />
+          Eine ausführliche Datenschutzerklärung erhalten Sie automatisch mit jeder Anmeldung zu Ausbildungen oder Workshops.
+        </p>
+        <p class="py-2">
+          Sollten Sie Fragen haben, können Sie sich jederzeit mit uns in Verbindung setzen:
+          <br />
+          E-Mail:<a class="font-bold pt-2 uppercase text-xs tracking-wide hover:text-ti_blue_accent ease-in-out transition-all duration-300" href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a>
+          <br />
+          oder telefonisch: +49 (0)221 683691
+          <br />
+          Ute Luckey,Geschäftsführung
+        </p>
+        <hr class="block border border-coolGray-100 w-full mt-4 mb-8" />
+        <h2 class="text-2xl uppercase text-ti_blue_mat tracking-wide mb-4">Google Analytics</h2>
+        <p>
+          Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website (einschließlich Ihrer IP-Adresse) wird an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten, die von Google gespeichert
+          werden, in Verbindung bringen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.
+          <br />
+          Die IP-Adresse, die an Google gesendet wird, wird zuvor anonymisiert, damit die gespeicherten Daten nicht mehr einer Person eindeutig zugeordnet werden können.
+        </p>
+      </DefaultCard>
+    </div>
+  </div>
+</section>

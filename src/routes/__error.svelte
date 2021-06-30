13aea42fbc8f254942ee3e3f10dbcbdb97c1b194
Schaeferjo <mail@joschaefer.com>
2021-06-30 16:26:41 +0200

    fix: replace ANNOYING upsi!!!

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,4 +1,5 @@
 <script context="module" lang="ts">
+  import { base } from '$app/paths';
   export function load({ error, status }) {
     return {
       props: {
@@ -20,12 +21,9 @@
   <title>{title}</title>
 </svelte:head>
 
-<div class="w-9/12 m-auto py-16 flex items-center justify-center">
-  <div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
-    <div class="border-t border-gray-200 text-center pt-8">
-      <h1 class="text-9xl font-bold text-purple-400">{status}</h1>
-      <h1 class="text-6xl font-medium py-8">Upsi!</h1>
-      <p class="text-2xl pb-8 px-12 font-medium">{error.message}</p>
-    </div>
+<div class="w-9/12 m-auto flex items-center justify-center">
+  <div class="bg-white p-12 shadow-ti overflow-hidden text-center">
+      <h2 class="text-2xl uppercase font-extrabold text-ti_blue_accent tracking-wide text-center mb-2">Error {status}</h2>
+      <p class="font-bold">{error.message}</p>
   </div>
 </div>

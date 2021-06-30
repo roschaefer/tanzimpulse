5da57041805aced333b95266b172bafe334d999a
Schaeferjo <mail@joschaefer.com>
2021-06-30 15:40:58 +0200

    feat: map styling and custom map marker

--- src/lib/components/VeranstaltungsOrt/VeranstaltungsOrt.svelte
+++ src/lib/components/VeranstaltungsOrt/VeranstaltungsOrt.svelte
@@ -7,14 +7,18 @@
       (async () => {
         const { koordinaten } = veranstaltungsort;
         const { latitude: lat, longitude: long } = koordinaten;
-
         const L = await import('leaflet');
+        const myIcon = L.icon({
+          iconUrl: '../mapmarker.png',
+          iconSize: [40, 50],
+          iconAnchor: [20,50],
+        });
         const mymap = L.map('mapid').setView([lat, long], 13);
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
-          maxZoom: 12
+          maxZoom: 19
         }).addTo(mymap);
-        L.marker([lat, long]).addTo(mymap);
+        L.marker([lat, long], {icon: myIcon}).addTo(mymap);
       })();
     });
   }
@@ -25,5 +29,5 @@
 </svelte:head>
 
 {#if veranstaltungsort?.koordinaten}
-  <div id="mapid" style="height: 400px; width: 100%;" />
+  <div id="mapid" style="width: 100%; height: 38vh; min-height:300px;" />
 {/if}

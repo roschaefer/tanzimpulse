beba464834167665e0893fbf79070a626b24df95
Schaeferjo <mail@joschaefer.com>
2021-07-06 16:54:47 +0200

    feat: styling fotogalerie and videos

--- src/routes/fotogalerie.svelte
+++ src/routes/fotogalerie.svelte
@@ -1,18 +1,51 @@
 <script context="module" lang="ts">
-  import { base } from '$app/paths';
-  import DefaultCard from '$lib/components/DefaultCard/DefaultCard.svelte';
+  import Carousel from '$lib/components/Slideshow/Carousel.svelte';
 </script>
 
 <svelte:head>
-  <title>Fotos</title>
+  <title>Fotogalerie</title>
 </svelte:head>
 
 <section>
   <div class="container">
-    <div class="grid grid-cols-1 gap-10">
-      <DefaultCard>
-        <h2 class="text-3xl uppercase font-bold text-ti_blue_accent tracking-wide mb-4">Fotos</h2>
-      </DefaultCard>
+    <div class="relative shadow-ti">
+      <Carousel perPage="1" autoplay="100000" easing="ease-in-out">
+        <span class="control" slot="left-control">
+          <svg xmlns="http://www.w3.org/2000/svg" class="relative w-18 h-18 text-white rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+          </svg>
+        </span>
+
+        <!-- single slide item -->
+        <div class="slide-content relative flex">
+          <img class="object-cover w-full" src="/images/slideshow/tanzraum.jpg" alt="Der Tanzraum von Tanzimpulse" />
+        </div>
+        <!-- -->
+
+        <!-- single slide item -->
+        <div class="slide-content relative flex">
+          <img class="object-cover w-full" src="/images/slideshow/tanzraum.jpg" alt="Der Tanzraum von Tanzimpulse" />
+        </div>
+        <!-- -->
+
+        <span class="control" slot="right-control">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-18 h-18 text-white rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </span>
+      </Carousel>
     </div>
   </div>
 </section>
+
+<style>
+  .slide-content {
+    width: 100%;
+    min-height: 400px;
+    max-height: 50vh;
+  }
+
+  .slide_text_box {
+    background: rgba(198, 20, 54, 0.9);
+  }
+</style>
--- src/routes/videos.svelte
+++ src/routes/videos.svelte
@@ -1,7 +1,3 @@
-<script context="module" lang="ts">
-  import { base } from '$app/paths';
-  import DefaultCard from '$lib/components/DefaultCard/DefaultCard.svelte';
-</script>
 
 <svelte:head>
   <title>Videos</title>
@@ -9,26 +5,25 @@
 
 <section>
   <div class="container">
-    <div class="grid grid-cols-1 gap-10">
-      <DefaultCard>
-        <h2 class="text-3xl uppercase font-bold text-ti_blue_accent tracking-wide mb-4">Videos</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-10">
 
-          <div class="aspect-w-16 aspect-h-9">
-            <iframe src="https://www.youtube.com/embed/sReA6W5qvIk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>         
-          </div>
+<div class="shadow-ti">
+  <div class="aspect-w-16 aspect-h-9">
+    <iframe src="https://www.youtube.com/embed/sReA6W5qvIk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>         
+  </div>
+</div>
+<div class="shadow-ti">
+  <div class="aspect-w-16 aspect-h-9">
+    <iframe src="https://www.youtube.com/embed/mngbGH_dJmE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</div>
+<div class="shadow-ti">
+  <div class="aspect-w-16 aspect-h-9">
+    <iframe src="https://www.youtube.com/embed/6EEsllb_gxc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</div>
 
-          <div class="aspect-w-16 aspect-h-9">
-            <iframe src="https://www.youtube.com/embed/mngbGH_dJmE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
 
-          <div class="aspect-w-16 aspect-h-9">
-            <iframe src="https://www.youtube.com/embed/6EEsllb_gxc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-        </div>
-
-
-      </DefaultCard>
     </div>
   </div>
 </section>

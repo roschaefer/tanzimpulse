2b4d53728c0617410b3cdc2fe881b43f88889b2b
Schaeferjo <mail@joschaefer.com>
2021-07-07 11:56:45 +0200

    fix: cols/ rows bug

--- src/lib/components/SeminarPreviews/SeminarPreviews.svelte
+++ src/lib/components/SeminarPreviews/SeminarPreviews.svelte
@@ -7,31 +7,23 @@
 </script>
 
 {#each seminare as seminar (seminar.id)}
-  <div class="flex flex-row lg:flex-col xl:flex-row gap-x-4 items-center">
-    <div class="flex-1">
+  <div class="flex flex-col md:flex-row lg:flex-row">
+    <div class="flex-grow	pt-2 pb-2 pl-2 pr-4">
       <p class="text-lg">{dateFormat.format(new Date(seminar.datum))}</p>
-
-      <p class="text-lg font-bold uppercase tracking-wide">{seminar.titel}</p>
-      
-        {#if overbooked(seminar)}
-        <p class="uppercase text-white text-sm tracking-wide">Anmeldung zur Warteliste </p>
-        {:else}
-        <p class="uppercase text-ti_green_accent-light text-sm tracking-wide">Noch Plätze frei!</p>
-        {/if}
-     
+      <p class="text-lg font-bold uppercase tracking-wide leading-6">{seminar.titel}</p>
+      {#if overbooked(seminar)}
+        <p class="uppercase text-white text-sm tracking-wide pt-2">Anmeldung zur Warteliste</p>
+      {:else}
+        <p class="uppercase text-ti_green_accent-light text-sm tracking-wide pt-2">Noch Plätze frei!</p>
+      {/if}
     </div>
-
-    <p>
+    <div class="flex-grow-0 flex mx-auto items-center p-2">
       <a sveltekit:prefetch href={`${base}/${seminar.format}/${seminar.url}`}>
         <Button buttonstyle={color}>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-          </svg></Button
-        ></a
-      >
-    </p>
-
+          </svg></Button></a>
+    </div>
   </div>
-  <hr class="block border border-black w-full my-4 opacity-20" />
+  <hr class="block border border-black w-full my-2 opacity-20" />
 {/each}
-

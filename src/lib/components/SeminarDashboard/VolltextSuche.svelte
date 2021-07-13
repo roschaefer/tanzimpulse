585b1d4f857536d73f6911d2aef3889e3edef6e2
Schaeferjo <mail@joschaefer.com>
2021-07-13 14:54:41 +0200

    feat: better lighthouse performance

--- src/lib/components/SeminarDashboard/VolltextSuche.svelte
+++ src/lib/components/SeminarDashboard/VolltextSuche.svelte
@@ -9,7 +9,7 @@
 
   <div class="relative w-full">
   <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-    <button type="submit" class="flex items-center px-4 py-4 focus:outline-none focus:shadow-outline">
+    <button aria-label="Nach Begriff suchen und sortieren" type="submit" class="flex items-center px-4 py-4 focus:outline-none focus:shadow-outline">
       <svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
     </button>
   </span>

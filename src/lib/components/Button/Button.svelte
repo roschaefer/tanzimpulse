24b82f5a658d86efe584784bf996acebc26823cb
Schaeferjo <mail@joschaefer.com>
2021-07-13 13:00:19 +0200

    feat: better ux experience

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -3,10 +3,10 @@
   export let href;
 
   const mapping = {
-    blue: 'text-ti_blue_accent-light border-ti_blue_accent-light hover:bg-ti_blue_mat-dark hover:text-white focus:bg-ti_blue_mat-dark focus:text-white',
-    cyan: 'text-ti_cyan_accent-light border-ti_cyan_accent-light hover:bg-ti_cyan_mat-dark hover:text-white focus:bg-ti_cyan_mat-dark focus:text-white',
-    green: 'text-ti_green_accent-light border-ti_green_accent-light hover:bg-ti_green_mat-dark hover:text-white focus:bg-ti_green_mat-dark focus:text-white ',
-    red: 'text-ti_red_accent-light border-ti_red_accent-light hover:bg-ti_red_mat-dark hover:text-white focus:bg-ti_red_mat-dark focus:text-white ',
+    blue: 'border-ti_blue_accent-light hover:bg-ti_blue_mat-dark hover:text-ti_blue_accent focus:text-ti_blue_accent focus:bg-ti_blue_mat-dark focus:text-white',
+    cyan: 'border-ti_cyan_accent-light hover:bg-ti_cyan_mat-dark hover:text-ti_cyan_accent focus:text-ti_cyan_accent focus:bg-ti_cyan_mat-dark focus:text-white',
+    green: 'border-ti_green_accent-light hover:bg-ti_green_mat-dark hover:text-ti_green_accent hover:text-ti_green_accent focus:bg-ti_green_mat-dark focus:text-white ',
+    red: 'border-ti_red_accent-light hover:bg-ti_red_mat-dark hover:text-ti_red_accent hover:text-ti_red_accent focus:bg-ti_red_mat-dark focus:text-white ',
     white: 'text-white border-white hover:bg-ti_red_mat-dark hover:text-white focus:bg-ti_red_mat-dark focus:text-white ',
     gray: 'text-gray-400 border-gray-400 cursor-not-allowed hover:text-gray-400 focus:text-gray-400'
   };

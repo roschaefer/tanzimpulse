61f06b5c4af3fc7150822bff05ea66f27e7bd967
Schaeferjo <mail@joschaefer.com>
2021-07-14 16:30:29 +0200

    feat: layout mailer

--- src/lib/mail/registration.ts
+++ src/lib/mail/registration.ts
@@ -1,34 +1,39 @@
 import { dateFormat } from "../helpers";
 const html = (teilnehmer: Teilnehmer, seminar: Seminar) => `
-  <p>
-    Hallo ${teilnehmer.name},</p>
-    <p>vielen Dank für Ihr Interesse zum Seminar "${seminar.titel}" am ${dateFormat.format(new Date(seminar.datum))}.</p>
-    <p>Damit Ihr Platz reserviert ist, überweisen Sie bitte bis zum xx den Betrag XX auf folgendes Konto:</p>
-    <p>IBAN: 123456<br>
+  <p>Hallo ${teilnehmer.name},</p>
+    <p>vielen Dank für Ihr Interesse zum Seminar "${seminar.titel}" am ${dateFormat.format(new Date(seminar.datum))}.
+    <br>Damit Ihr Platz reserviert ist, überweisen Sie bitte innerhalb von 14 Tagen, spätestens jedoch bis zum Veranstaltungstermin, den Betrag von ${seminar.preis} Euro auf folgendes Konto:</p>
+    <p>IBAN: 12345689<br>
     Musterbank</p>
-    <p>Erst nach Eingang der Kursgebühr auf unserem Konto ist Ihr Platz reserviert.</p>
-    <p>Bitte haben Sie dafür Verständnis, dass bei Nichteingang des Betrages innerhalb der oben genannten Frist, wir Ihre Anfrage stornieren und den Platz wieder für andere Interessenten freigeben</p>
+    <p><strong>Erst nach Eingang der Kursgebühr auf unserem Konto ist Ihr Platz reserviert.</strong>
+    <br>Bitte haben Sie dafür Verständnis, dass bei Nichteingang des Betrages innerhalb der oben genannten Frist, wir Ihre Anfrage stornieren und den Platz wieder für andere Interessenten freigeben</p>
     <p>Wir freuen uns auf Ihre Teilnahme und verbleiben mit freundlichen Grüßen</p>
     <p>Ihr Tanzimpulse-Team</p>
+    <br>
     <hr>
-    <p><strong>Tanzimpulse - Institut für Tanzimpulse<strong></p>
-    <p>Büroanschrift<br>
-        Folwiese 20<br>
-51069 Köln<br>
-Bettina Bierdümpel und Ute Luckey</p>
-<br>
-<p>
-
-<p>Kontakt<br>
-Tel:		0221 - 68 36 91<br>
-Fax: 	0221 - 689 77 97<br>
-Web: 	<a href="https://www.tanzimpulse.de" target="_blank">tanzimpulse.de</a>
-</p>
- 
+    <br>
+    <p><strong>Tanzimpulse - Institut für Tanzimpulse</strong></p>
+    <p>
+    Büroanschrift<br>
+    Folwiese 20<br>
+    51069 Köln<br>
+    Bettina Bierdümpel und Ute Luckey</p>
+    <p>Kontakt<br>
+    Tel:	0221 - 68 36 91<br>
+    Fax: 	0221 - 689 77 97<br>
+    E-Mail: <a href="mailto:info@tanzimpulse.de">info@tanzimpulse.de</a><br>
+    Web: 	<a href="https://www.tanzimpulse.de" target="_blank">tanzimpulse.de</a>
+    </p>
+    <p>
+    Bürozeiten<br>
+    Montag bis Freitag: 8:30 - 13:00 Uhr<br >
+    Dienstags und Donnerstags: 20:30 - 22:00 Uhr
+    </p>
+      
 
 
     ${seminar.kurzbeschreibung}
-  </p>
+
 `;
 
 const text = (teilnehmer: Teilnehmer, seminar: Seminar) => html(teilnehmer, seminar).replace(/(<([^>]+)>)/gi, '');
--- src/lib/mail/waitingList.ts
+++ src/lib/mail/waitingList.ts
@@ -7,19 +7,27 @@ Wir haben Sie auf die Warteliste gesetzt und werden Sie kontaktieren, wenn kurzf
 </p>
 <p>Mit freundlichen Grüßen</p>
 <p>Ihr Tanzimpulse-Team</p>
+<br>
 <hr>
-    <p><strong>Tanzimpulse - Institut für Tanzimpulse</strong></p>
-    <p>Büroanschrift<br>
-        Folwiese 20<br>
+<br>
+<p><strong>Tanzimpulse - Institut für Tanzimpulse</strong></p>
+<p>
+Büroanschrift<br>
+Folwiese 20<br>
 51069 Köln<br>
 Bettina Bierdümpel und Ute Luckey</p>
-
-
 <p>Kontakt<br>
-Tel:		0221 - 68 36 91<br>
+Tel:	0221 - 68 36 91<br>
 Fax: 	0221 - 689 77 97<br>
+E-Mail: <a href="mailto:info@tanzimpulse.de">info@tanzimpulse.de</a><br>
 Web: 	<a href="https://www.tanzimpulse.de" target="_blank">tanzimpulse.de</a>
 </p>
+<p>
+Bürozeiten<br>
+Montag bis Freitag: 8:30 - 13:00 Uhr<br >
+Dienstags und Donnerstags: 20:30 - 22:00 Uhr
+</p>
+  
 `;
 
 const text = (teilnehmer: Teilnehmer, seminar: Seminar) => html(teilnehmer, seminar).replace(/(<([^>]+)>)/gi, '');
